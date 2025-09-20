55dd517d37c82d480b2068442511a1ebf06d2f90
LUAN LIMA FERREIRA <ferreira.luan@aluno.senac.rio>
2025-09-19 21:33:06 -0300

    atualização ex1

--- exercicio1.py
+++ exercicio1.py
@@ -2,7 +2,11 @@
 funcionariosParaCadastrar = [
     {"nome": "Pablo", "sobrenome": "Araujo", "idade": 34, "altura": 1.71, "temHabilitacao": True},
     {"nome": "Ana", "sobrenome": "Silva", "idade": 28, "altura": 1.65, "temHabilitacao": False},
-    {"nome": "Carlos", "sobrenome": "Souza", "idade": 40, "altura": 1.80, "temHabilitacao": True}
+    {"nome": "Carlos", "sobrenome": "Souza", "idade": 40, "altura": 1.80, "temHabilitacao": True},
+    {"nome": "Luan", "sobrenome": "Lima", "idade": 23, "altura": 1.61, "temHabilitacao": True},
+    {"nome": "Luan", "sobrenome": "Lima", "idade": 23, "altura": 1.61, "temHabilitacao": True},
+    {"nome": "Ruan", "sobrenome": "Barros", "idade": 21, "altura": 1.65, "temHabilitacao": False},
+    {"nome": "Rhyan", "sobrenome": "Henrique", "idade": 18, "altura": 1.70, "temHabilitacao": False}
 ]
 
 cadastrosParaEnviarParaOBanco = []
@@ -27,11 +31,15 @@ def cadastrar_funcionarios(lista_de_dicionarios):
             temHabilitacao=dados["temHabilitacao"]
         )
         cadastrosParaEnviarParaOBanco.append(pessoa)
+        
 
 
 def salvar_cadastros(lista_de_pessoas):
     for pessoa in lista_de_pessoas:
         print(f"O usuário {pessoa.nome} {pessoa.sobrenome} foi salvo com sucesso.")
+        
+        
+
 
 
 cadastrar_funcionarios(funcionariosParaCadastrar)
