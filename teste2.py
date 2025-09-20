a9f7e91d40fc6f3f78bda7c2e5ac110a5ccc1782
LUAN LIMA FERREIRA <ferreira.luan@aluno.senac.rio>
2025-09-19 21:17:07 -0300

    alteração1

--- teste2.py
+++ teste2.py
@@ -1,3 +1,29 @@
 nome = input('Qual é o seu nome? ')
 
-print('Olá, ', nome)
+print('Olá,', nome)
+
+idade = int(input('Quantos anos você tem? '))
+
+alt = float(input('Qual a sua altura? '))
+
+peso = float(input('E qual é o seu peso(em kg)? '))
+
+
+imc = peso / (alt ** 2)
+
+
+if imc == 0:
+    print('Digite um peso e uma altura válida.')
+    
+
+elif imc >= 30:
+    print('Cuidado, seu indíce de massa corporal indica que está com obesidade')
+    
+elif imc >= 25 and imc <= 29.99:
+    print('Seu índice de massa corporal indica que você está com sobrepeso')
+    
+elif imc >= 18.5 and imc <=24.99:
+    print('Seu índice de massa corporal indica que você está no peso ideal')
+    
+elif imc < 18.5:
+    print('Seu índice de massa corporal indica que você está abaixo do peso')
